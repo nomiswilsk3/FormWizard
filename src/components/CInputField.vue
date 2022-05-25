c96ad80f19a8abf5e65b4da44371148b0ac698db
Wilson Kafuko <nomiswilsk3@gmail.com>
2022-05-26 00:56:36 +0300

    Creates simple reusable components i.e CButtonWide for a bit wide buttons,CInputField for input fields, CRadioField for radio buttons input fields

---
+++ src/components/CInputField.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  interface Props {
+    label: string,
+    input_type?: string,
+    min?: number,
+    max?: number,
+    placeholder?: string,
+  }
+
+  const props = defineProps<Props>()
+  const emit = defineEmits(['input'])
+
+  let inputValue = ref<String | null>()
+
+</script>
+
+<template>
+  <div>
+    <label>{{props.label}}</label>
+    <input 
+      :type="props.input_type || 'text'"
+      :min="props.min"
+      :max="props.max"
+      :placeholder="(props.placeholder)? props.placeholder :''" 
+      v-model="inputValue" @input="() => emit('input',inputValue)"
+    />
+  </div>
+</template>
+
+<style scoped>
+  label {
+    text-align: left !important;
+    float: left;
+  }
+  input{
+    height:25px;
+    border:0px;
+    width: 100%;
+    border:  1px solid rgb(209, 202, 202);
+    margin-bottom:10px
+  }
+</style>
---
+++ src/components/CRadioField.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+    interface Package {
+        id:string,
+        name:string
+    }
+
+  interface Props {
+    label: string,
+    option_values: Package []
+  }
+
+  const props = defineProps<Props>()
+  const emit = defineEmits(['change'])
+
+  let picked = ref<String | null>()
+
+</script>
+
+<template>
+<label class="container" v-for="prop in props.option_values" :key="prop.id"> {{prop.name}}
+  <input type="radio" :id="prop.id" :value="prop.id" v-model="picked" v-on:change="(e) => emit('change',picked)">
+  <span class="checkmark"></span>
+</label>
+</template>
+
+<style scoped>
+  .container {
+    display: block;
+    position: relative;
+    padding-left: 0px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    font-size: 15px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  .container input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 15px;
+    width: 15px;
+    background-color: #eee;
+    border-radius: 50%;
+    font-size: 12px;;
+  }
+  .container:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+  .container input:checked ~ .checkmark {
+    background-color: #3a45af;
+  }
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+  .container input:checked ~ .checkmark:after {
+    display: block;
+  }
+  .container .checkmark:after {
+    top: 5px;
+    left: 5px;
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    background: white;
+  }
+</style>
